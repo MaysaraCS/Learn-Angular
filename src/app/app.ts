bb89aae14e36f367750fbac98115ce8150139268
Maysara <maysaracs1001@gmail.com>
2025-09-08 17:36:06 +0300

    componenets 3

--- src/app/app.ts
+++ src/app/app.ts
@@ -11,4 +11,13 @@ import { Component, signal } from '@angular/core';
 })
 export class AppComponent {
   //protected readonly title = signal('angular-basics');
+  name = signal ('Maysara');
+  imageURL = signal('https://angular.io/assets/images/logos/angular/angular.png');
+
+  changeImage(e: KeyboardEvent){
+    this.imageURL.set((e.target as HTMLInputElement).value);
+  }
+  getName(){
+    return this.name();
+  }
 }
