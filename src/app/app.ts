6ef5f48fad792730db0dfb755ca2801c614325a2
Maysara <maysaracs1001@gmail.com>
2025-09-08 15:58:12 +0300

    componenets 1

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  template: `<h1>Hello World</h1>`,
+  standalone: true,
+  // imports: [RouterOutlet],
+  // templateUrl: './app.html',
+  // styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('angular-basics');
+export class AppComponent {
+  //protected readonly title = signal('angular-basics');
 }
